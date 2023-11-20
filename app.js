36babdb11b39af7b2fee35f318033417edef0ecd
JhussyaraReis <147738753+JhussyaraReis@users.noreply.github.com>
2023-11-20 12:45:25 -0300

    add authenticate

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
 require("./models/Categoria");
 const Categoria = mongoose.model("categorias");
+require("./config/auth")(passport);
 
 /**     Config
  *        Session     */
@@ -22,12 +24,15 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 /*       Middleware     */
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
